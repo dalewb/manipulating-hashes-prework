dd07922da9b8acf9b0994a3853c8293dfa5e1875
William Dale <williambdale@gmail.com>
2018-03-09 14:04:10 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |name, data|
+    data.each do |data, info|
+      
+    end 
+  end 
 
 
   #remember to return your newly altered contacts hash!
